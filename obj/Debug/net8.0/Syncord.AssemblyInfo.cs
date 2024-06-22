06b4acc1546fc7636388e3a66c988cf1d82f6574
FadyBoj <fadynabil701@gmail.com>
2024-06-22 18:14:58 +0300

    Restoring

--- obj/Debug/net8.0/Syncord.AssemblyInfo.cs
+++ obj/Debug/net8.0/Syncord.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Syncord")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f25508938afc0044b897df68dbe67ddcb581ab1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c372b2211d55792aadfea128fefc355874bde9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Syncord")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Syncord")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
